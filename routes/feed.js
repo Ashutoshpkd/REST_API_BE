2e52410a770e841c0f2d3ad84c008926fd923dab
Ashutosh Pawade <ashpawad@WKMZTE2DB86B.global.publicisgroupe.net>
2022-12-15 21:03:08 +0530

    Adding auth check to all the routes

--- routes/feed.js
+++ routes/feed.js
@@ -5,11 +5,11 @@ const { authCheck } = require('../middleware/auth');
 
 const router = express.Router();
 
-router.get('/posts/:postId', feedController.getPostById);
+router.get('/posts/:postId', authCheck, feedController.getPostById);
 
 router.put('/post/:postId', authCheck, feedController.updatePost);
 
-router.get('/posts', feedController.getPosts);
+router.get('/posts', authCheck, feedController.getPosts);
 
 router.post('/post', authCheck, feedController.createPost);
 
